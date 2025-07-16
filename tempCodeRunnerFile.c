e68a631836e04ccfaef4379fa94e5f0bedb35bf8
Aadarsh01123 <aadarshjayesh@gmail.com>
2025-07-16 09:06:11 +0530

    add c files

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int sum = a + b;
+    printf("Enter two integers: ");
+    printf("%d + %d = %d\n", a, b, sum);
+    return 0;
+}
