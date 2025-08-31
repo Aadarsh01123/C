cfdcb68803fea15f5846228511b2e51f1679c155
Aadarsh01123 <aadarshjayesh@gmail.com>
2025-08-31 07:52:27 +0530

    findSimple_interest.c

---
+++ findSimple_interest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    float p, r, t, si;
+    printf("Enter the principle amount :");
+    scanf("%f", &p);
+    printf("Enter the rate of interest :");
+    scanf("%f", &r);
+    printf("Enter the time in years :");
+    scanf("%f", &t);
+    si = (p * r * t) / 100;
+    printf("simple interest is : %f", si);
+    return 0;
+}
